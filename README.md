1f4336f04f756cb39d364288bf95b54e694f41af
Zhouchanglong <changlong.zhou@56qq.com>
2019-10-31 17:38:53 +0800

    feat: 添加错误页面模板

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # blog-site
+
+### 创建虚拟环境
+python3 -m venv venv
+
+### 激活虚拟环境
+1. mac: . venv/bin/activate
+2. windows: venv\Scripts\activate
--- manage.py
+++ manage.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
-    return 'Welcome to Flask'
+    return render_template('index.html', name = 'Joe.C.Zhou'), 200
 
 @app.route('/index')
 def user():
     return 'Hello Flask'
+
+@app.errorhandler(404)
+def not_found():
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def not_found():
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def not_found():
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
